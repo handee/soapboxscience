c0ccfed72760eb8f205eb17501b2a274a4fdc4aa
hannah d <hmd@hannahdee.eu>
2016-05-31 18:10:25 +0100

    updated for rotates and page numbers

--- makebook.py
+++ makebook.py
@@ -1,41 +1,69 @@
 import os
 import sys
 
+if (len(sys.argv)<3):
+    print "Needs two arguments, both directories full of images\n";
+else:
 
-directory1=os.path.abspath(os.path.join(os.getcwd(), sys.argv[1]))
-directory2=os.path.abspath(os.path.join(os.getcwd(), sys.argv[2]))
+    directory1=os.path.abspath(os.path.join(os.getcwd(), sys.argv[1]))
+    directory2=os.path.abspath(os.path.join(os.getcwd(), sys.argv[2]))
 
-files2=os.listdir(directory2)
-files1=os.listdir(directory1)
+    files2=os.listdir(directory2)
+    files1=os.listdir(directory1)
 
-#sort files1 ascending, sort files2 descending
+    #sort files1 ascending, sort files2 descending
 
-files1.sort()
-files2.sort()
-files2.reverse()
+    files1.sort()
+    files2.sort()
+    files2.reverse()
 
-print("\\documentclass{minimal}\n\n")
-print("\\usepackage{graphicx}\n\n")
+    print("\\documentclass{minimal}\n\n")
+    print("\\usepackage{graphicx}\n\n")
 
-print("\\begin{document}")
-print("\\clearpage")
-#iterate by id
-for i in range(0,len(files1)):
-    #multiple copies of file1
-    print("\\begin{tabular}{ c }")
-    print("\\includegraphics[width=12cm]{%s/%s} \\\\" %(sys.argv[1], files1[i]))
-    print("\\includegraphics[width=12cm]{%s/%s} \\\\" %(sys.argv[1], files1[i]))
-    print("\\includegraphics[width=12cm]{%s/%s} \\\\" %(sys.argv[1], files1[i]))
-    print("\\end{tabular}")
-    #pagebreak
+    print("\\begin{document}")
     print("\\clearpage")
-    #multiple copies of file2
-    print("\\begin{tabular}{ c }")
-    print("\\includegraphics[width=12cm]{%s/%s} \\\\" %(sys.argv[2], files2[i]))
-    print("\\includegraphics[width=12cm]{%s/%s} \\\\" %(sys.argv[2], files2[i]))
-    print("\\includegraphics[width=12cm]{%s/%s} \\\\" %(sys.argv[2], files2[i]))
-    print("\\end{tabular}")
-    print("\\clearpage")
+    #iterate by id
+    for i in range(0,len(files1)):
+        #multiple copies of file1
+        print("\\begin{tabular}{| c |}")
+        print("\\hline")
+        print("%d\\\\" %i)
+        print("\\hline")
+        print("\\includegraphics[width=10cm]{%s/%s} \\\\" %(sys.argv[1], files1[i]))
+        print("\\hline")
+        print("\\hline")
+        print("%d\\\\" %i)
+        print("\\hline")
+        print("\\includegraphics[width=10cm]{%s/%s} \\\\" %(sys.argv[1], files1[i]))
+        print("\\hline")
+        print("\\hline")
+        print("%d\\\\" %i)
+        print("\\hline")
+        print("\\includegraphics[width=10cm]{%s/%s} \\\\" %(sys.argv[1], files1[i]))
+        print("\\hline")
+        print("\\end{tabular}")
+        #pagebreak
+        print("\\newpage")
+        #multiple copies of file2
+        print("\\begin{tabular}{| c |}")
+        print("\\hline")
+        print("%d\\\\" %i)
+        print("\\hline")
 
-print("\\end{document}")
+        print("\\includegraphics[width=10cm,angle=180]{%s/%s} \\\\" %(sys.argv[2], files2[i]))
+        print("\\hline")
+        print("\\hline")
+        print("%d\\\\" %i)
+        print("\\hline")
+        print("\\includegraphics[width=10cm,angle=180]{%s/%s} \\\\" %(sys.argv[2], files2[i]))
+        print("\\hline")
+        print("\\hline")
+        print("%d\\\\" %(i))
+        print("\\hline")
+        print("\\includegraphics[width=10cm,angle=180]{%s/%s} \\\\" %(sys.argv[2], files2[i]))
+        print("\\hline")
+        print("\\end{tabular}")
+        print("\\newpage")
+
+    print("\\end{document}")
 
